08ad28c59710e8a70e0540b6677c495f72480189
aprusinowski <artur.prusinowski@gmail.com>
2016-10-12 14:55:59 -0700

    Almost Final

--- file_parser.cc
+++ file_parser.cc
@@ -1,3 +1,4 @@
+
 #include "file_parser.h"
 
 file_parser::file_parser(string f_name) {
@@ -7,8 +8,8 @@ file_parser::file_parser(string f_name) {
 
 void file_parser::read_file() {
     string line;
-    std::ifstream f_input(filename.c_str());
-    if (!f_input)
+    ifstream f_input(filename.c_str());
+    if (!f_input.is_open())
         throw file_parse_exception("Could not open file: " + filename);
     while (getline(f_input, line))
         contents.push_back(line);
@@ -33,6 +34,7 @@ string file_parser::get_token(unsigned int row, unsigned int col) {
 }
 
 void file_parser::tokenize_lines() {
+    char charary[] = " \t\n\r";
     unsigned int field;
     string::size_type pos, last_pos, end_quote;
     //Iterate over the rows
@@ -54,22 +56,23 @@ void file_parser::tokenize_lines() {
             }
             //if read in max number of fields already throw an exception since the token is not a comment
             if (field == MAX_FIELDS)
-                throw file_parse_exception("Too many tokens on line: " + to_string(number_of_lines + 1));
-
-            //process tokens containing chars in single quotes
+                throw file_parse_exception("Too many tokens on line: " + to_string(tokens.size() + 1));
+                //process tokens containing chars in single quotes
             else if (token_str.front() == SINGLE_QUOTE && pos < ULONG_MAX) {
                 end_quote = row.find_first_of(SINGLE_QUOTE, pos);
-                token_str = row.substr(last_pos, end_quote - 1);
+                token_str = row.substr(last_pos, end_quote - last_pos);
+                string::size_type eot = row.find_first_of(" \n\r\t", end_quote);
+                token_str = token_str += row.substr(end_quote, eot-end_quote);
                 new_row[field++] = token_str;
-                pos = last_pos + end_quote;
-
-            //check for invalid label
+                pos = (string::npos == eot)? eot : end_quote+(eot-end_quote);
+             //check for invalid label
             } else if (last_pos == 0 && field == LABEL && !is_valid_label(token_str))
-                throw file_parse_exception("Invalid label on line: " + to_string(number_of_lines + 1));
+                throw file_parse_exception("Invalid label on line: " + to_string(tokens.size()  + 1));
             else
                 //store token in the next field position. If label, truncate to 8 chars max
-                new_row[field++] = (field == LABEL)?token_str.substr(0,8):token_str;
+                new_row[field++] = token_str;
 
+            //find start and end of the next token on line
             last_pos = row.find_first_not_of(DELIMITER, pos);
             pos = row.find_first_of(DELIMITER, last_pos);
         }
@@ -92,3 +95,181 @@ bool file_parser::is_valid_label(string s) {
     return true;
 }
 
+
+
+
+
+
+
+
+//#include "file_parser.h"
+//
+//file_parser::file_parser(string filename) {
+//    file_name = filename;
+//    number_of_lines = 0;
+//}
+//
+//file_parser::~file_parser() {}
+//
+//void file_parser::read_file() {
+//    ifstream input; //opens an ifstream to read from file
+//    string line;
+//    row r;
+//    input.open(file_name.c_str(), ios_base::in); // opens the file to read from
+//    if (!input) // if file couldn't be opened throw exception
+//        throw file_parse_exception("couldn't open file, please check arguments");
+//    while (!input.eof()) { // read the file while there is still data
+//        getline(input, line); // get a line of data and store it in line.
+//        token_lines.push_back(line); // add it to the vector of lines.
+//        r = tokenize(line);     // tokenize the line into 1-4 tokens
+//        number_of_lines++;      // increment the number of lines tokenized
+//        file_rows.push_back(r);      // add the tokenized row to the vector of file_rows
+//        if(number_of_lines == 37)
+//            int a = 5;
+//    }
+//    input.close();              // close the file
+//}
+//
+//void file_parser::print_file() {
+//    for (row r: file_rows) {
+//        printf("%-2s\t %-2s\t %-2s\t %s \n", (r.label).c_str(), r.opcode.c_str(), r.operand.c_str(),
+//               r.comments.c_str());
+//
+//    }
+//}
+//
+//string file_parser::get_token(unsigned int row, unsigned int column) {
+//    if (row > file_rows.size() - 1)
+//        throw file_parse_exception("The provided row is out of the range of the file, please try again");
+//    if (column > 4)
+//        throw file_parse_exception(
+//                "The provided column is out of the range of the file, please choose a number between 0-4");
+//    switch (column) {
+//        case 0:
+//            return file_rows[row].label;
+//            break;
+//        case 1:
+//            return file_rows[row].opcode;
+//            break;
+//        case 2:
+//            return file_rows[row].operand;
+//            break;
+//        case 3:
+//            return file_rows[row].comments;
+//            break;
+//    }
+//    return " ";
+//}
+//
+//file_parser::row file_parser::tokenize(string str) {
+//    const string &delimiters = "  \t";
+//    row r;
+//    string::size_type lastpos = str.find_first_not_of(delimiters, 0); // Skip delimiters at beginning.
+//    string::size_type pos = str.find_first_of(delimiters, lastpos); // Find first "delimiter".
+//    int num_tokens = 0; //number of tokens on the line;
+//    while (string::npos != pos || string::npos != lastpos) {
+//        if (IS_COMMENT(str[lastpos])) {
+//            add_comment(r, str, lastpos, num_tokens);
+//            break;
+//        }
+//        else if (IS_LABEL(str[pos - 1]) || IS_LABEL(*str.rbegin()) || IS_INCLUDE(str[lastpos]))  //if label
+//            add_label(r, str, lastpos, pos, num_tokens, delimiters);
+//        else if (IS_SPACE(str[lastpos - 1])) { // if opcode and operand
+//            add_opcode(r, str, lastpos, pos, num_tokens, delimiters);
+//            if (IS_COMMENT(str[lastpos])) {
+//                add_comment(r, str, lastpos, num_tokens);
+//                break;
+//            }
+//            add_operand(r, str, lastpos, pos, num_tokens, delimiters);
+//        }
+//        check_invalid_tokens(num_tokens, r);
+//        lastpos = str.find_first_not_of(delimiters, pos);
+//        pos = str.find_first_of(delimiters, lastpos);
+//    }
+//    return r;
+//}
+//
+//void file_parser::check_label(string str, size_t pos) {
+//    string::iterator it = str.begin();
+//    if (IS_SPACE(str[0])) {
+//        stringstream ss;
+//        ss << "the label must to be on column 0, line " << number_of_lines + 1;
+//        string message = ss.str();
+//        throw file_parse_exception(message);
+//    }
+//    if (!isalpha(str[0])) {
+//        stringstream ss;
+//        ss << "The 1st letter of the label has to be a letter, on line " << number_of_lines + 1;
+//        string message = ss.str();
+//        throw file_parse_exception(message);
+//    }
+//    while (!IS_LABEL(*it)) {
+//        if (!isalnum(*it)) {
+//            stringstream ss;
+//            ss << "The lable has to be alphanumeric, on line " << number_of_lines + 1;
+//            string message = ss.str();
+//            throw file_parse_exception(message);
+//        }
+//        it++;
+//    }
+//
+//}
+//
+//void file_parser::check_invalid_tokens(int tokens, row r) {
+//    if ((r.label.empty() && r.comments.empty() && tokens >= 3)) {
+//        stringstream ss;
+//        ss << "invalid number of tokens on line  " << number_of_lines + 1;
+//        string message = ss.str();
+//        throw file_parse_exception(message);
+//    }
+//}
+//
+//void file_parser::add_comment(row &r, string str, string::size_type lastpos, int &num_tokens) {
+//    r.comments = str.substr(lastpos);
+//    num_tokens++;
+//}
+//
+//void file_parser::add_label(file_parser::row &r, string str, string::size_type &lastpos, string::size_type &pos, int num_tokens,
+//                            const string &delimiters) {
+//    if(IS_INCLUDE(str[lastpos])){
+//        add_include(r,str,lastpos,pos,num_tokens,delimiters);
+//    }
+//    else {
+//        check_label(str, pos);
+//        r.label = str.substr(lastpos, pos - lastpos);
+//        num_tokens++;
+//    }
+//}
+//
+//void
+//file_parser::add_opcode(row &r, string str, string::size_type &lastpos, string::size_type &pos, int &num_tokens, const
+//string &delimiters) {
+//    r.opcode = str.substr(lastpos, pos - lastpos);
+//    num_tokens++;
+//    lastpos = str.find_first_not_of(delimiters, pos);
+//    pos = str.find_first_of(delimiters, lastpos);
+//}
+//
+//void
+//file_parser::add_operand(row &r, string str, string::size_type &lastpos, string::size_type &pos, int &num_tokens, const
+//string &delimiters) {
+//    if (IS_Quotes(str[lastpos])) {
+//        pos = str.find_first_of("\'", lastpos + 1);
+//        r.operand = str.substr(lastpos, pos - lastpos);
+//        lastpos = str.find_first_not_of(delimiters, pos);
+//        pos = str.find_first_of(delimiters, lastpos);
+//        r.operand += str.substr(lastpos, pos - lastpos);
+//
+//    } else if (lastpos <= str.length())
+//        r.operand = str.substr(lastpos, pos - lastpos);
+//    num_tokens++;
+//    cout<<r.operand<<endl;
+//}
+//
+//void file_parser::add_include(row &r, string str, string::size_type &lastpos, string::size_type &pos, int &num_tokens,
+//                              const string &delimiters) {
+//    r.label = str.substr(lastpos, pos - lastpos);
+//    lastpos = str.find_first_not_of(delimiters, pos);
+//    pos = str.find_first_of(delimiters, lastpos);
+//    r.opcode = str.substr(lastpos, pos - lastpos);
+//}
--- file_parser.h
+++ file_parser.h
@@ -78,3 +78,122 @@ private:
 };
 
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#ifndef FILE_PARSER_H
+//#define FILE_PARSER_H
+//#define  IS_COMMENT(C)      (C == '*' || C == ';')
+#define  IS_SPACE(C)        (C == ' ' || C == '\t')
+#define  IS_LABEL(C)        (C == ':')
+#define  IS_INCLUDE(C)      (C == '#')
+#define  IS_Quotes(C)       (C == '\'')
+
+//#include <string>
+//#include <vector>
+//#include <iostream>
+//#include <fstream>
+//#include <iomanip>
+//#include <cstdlib>
+//#include <cstring>
+//#include "file_parse_exception.h"
+//#include <sstream>
+//
+//using namespace std;
+//
+//class file_parser {
+//
+//public:
+//    // ctor, filename is the parameter.  A driver program will read
+//    // the filename from the command line, and pass the filename to
+//    // the file_parser constructor.  Filenames must not be hard-coded.
+//    file_parser(string);
+//
+//    // dtor
+//    ~file_parser();
+//
+//    // reads the source file, storing the information is some
+//    // auxiliary data structure you define in the private section.
+//    // Throws a file_parse_exception if an error occurs.
+//    // if the source code file fails to conform to the above
+//    // specification, this is an error condition.
+//    void read_file();
+//
+//    // returns the token found at (row, column).  Rows and columns
+//    // are zero based.  Returns the empty string "" if there is no
+//    // token at that location. column refers to the four fields
+//    // identified above.
+//    string get_token(unsigned int, unsigned int);
+//
+//    // prints the source code file to stdout.  Should not repeat
+//    // the exact formatting of the original, but uses tabs to align
+//    // similar tokens in a column. The fields should match the
+//    // order of token fields given above (label/opcode/operands/comments)
+//    void print_file();
+//
+//    // returns the number of lines in the source code file
+//    int size();
+//
+//private:
+//    //returns a vector of file_rows from a vector of strings
+//    struct row{
+//        string label;
+//        string opcode;
+//        string operand;
+//        string comments;
+//    };
+//
+//    file_parser::row tokenize(string);
+//    void check_label(string, string::size_type);
+//    void check_invalid_tokens(int,row);
+//    void add_comment(row &,string,string::size_type, int &);
+//    void add_label(file_parser::row &, string, string::size_type& , string::size_type &, int, const string &basicString);
+//    void add_opcode(row &,string,string::size_type &,string::size_type &,int &,const string&);
+//    void add_operand(row &,string,string::size_type &,string::size_type &,int &,const string&);
+//    void add_include(row &,string,string::size_type &,string::size_type &,int &,const string&);
+//
+//
+//    string file_name;
+//    vector<file_parser::row> file_rows;
+//    vector<string> token_lines;
+//
+//    int number_of_lines;
+//
+//
+//
+//};
+//
+//#endif
+
